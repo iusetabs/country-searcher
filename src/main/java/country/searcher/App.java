bdb6ed4e95013e352766281032b25a4e08a347e7
James <james.nolan38@mail.dcu.ie>
2019-04-20 14:31:40 +0100

    REST running. Working on basic client

--- src/main/java/country/searcher/App.java
+++ src/main/java/country/searcher/App.java
@@ -5,8 +5,9 @@ package country.searcher;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude={DataSourceAutoConfiguration.class})
 public class App {
     public String getGreeting() {
         return "Hello world.";
